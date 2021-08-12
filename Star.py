a847038d075d2feea5ee5130af4eba21a1c99ca0
Jordan Iga <jordaniga@MacBook-Pro.local>
2021-08-12 11:31:58 +0100

    Improved Star class ease of use

--- Star.py
+++ Star.py
@@ -19,32 +19,47 @@ R_SOLAR = 6.957E10  # Solar radius in cm
 
 class Star(object):
     """
-    A class to define a white dwarf (WD) system.
+    A class to define a stellar object.
 
     Properties:
-    label: A label for the system
-    mass: The Mass of the WD in grams
-    radius: The radius of the WD in cm
-    t_eff: The effective temperature of the WD in K
-    distance: The distance of the system from the sun in cm
+    label: A label for the star.
+    mass: The Mass of the object in in Solar masses.
+    radius: The radius of the object in Solar radii.
+    t_eff: The effective temperature of the object in K.
+    distance: The distance of the system from the sun in parsec.
 
     Methods:
     _init_
-    set_radius: Determines and sets the radius of a WD from its surface gravity
+    set_radius: Determines and sets the radius of a object from its surface gravity.
+    radius_cgs: Returns the radius of the star in cm.
+    mass_cgs: Returns the mass of the star in grams.
+    distance_cgs: Returns the distance of the star in cm.
 
     """
 
     def __init__(self, label, mass, radius, t_eff, distance):
 
         self.label = label
-        self.mass = mass*M_SOLAR
-        self.radius = radius*R_SOLAR
+        self.mass = mass
+        self.radius = radius
         self.t_eff = t_eff
-        self.distance = distance * PARSEC
+        self.distance = distance
 
     def set_radius(self, log_g):
 
         self.radius = np.sqrt((G*self.mass)/(10**log_g))
+
+    def radius_cgs(self):
+
+        return self.radius*R_SOLAR
+
+    def mass_cgs(self):
+
+        return self.mass*M_SOLAR
+
+    def distance_cgs(self):
+
+        return self.distance*PARSEC
 
 
 
